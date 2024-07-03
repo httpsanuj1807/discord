2707a36c18023afdb1c8a176b343c7707c8fd9ad
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-07-03 15:13:07 +0530

    initialised files and folders

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return(
+        <div>
+            <p>This is login page!</p>
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return(
+        <div>
+            <p>This is register page!</p>
+        </div>
+    );
+}
+
+export default LoginPage;
